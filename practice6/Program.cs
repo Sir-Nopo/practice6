ee4f3bd13ba5bb9c77468ce441772f6a3d411166
SirGoogle <SirGoogle@DESKTOP-V6QGN9V>
2023-11-01 12:46:01 +0330

    Add project files.

---
+++ practice6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace practice6
+{
+    class Program
+    {
+        //MohammadRezaFatahi
+        //tamrin6
+        static void Main(string[] args)
+        {
+        first:
+            Console.WriteLine("adad mored nazar ra baraye peyda kardan donbale vared konid :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = 0;
+            for (int i = 1; i <= a; i++)
+                b += i;
+
+            Console.WriteLine(b);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("aya mikhay edame bedi?! (y/n)");
+            string m = Console.ReadLine();
+            if (m == "y")
+            {
+                goto first;
+            }
+            else
+            {
+                Console.WriteLine("khodafez");
+            }
+        }
+    }
+}
